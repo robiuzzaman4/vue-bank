7b21f5f8efb2df1077f9fa868f40d8b1970de3d4
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-09-09 22:20:51 +0600

    feat: deposite and withdraw section input and button design done

--- src/components/Bank.vue
+++ src/components/Bank.vue
@@ -10,7 +10,7 @@ const availableBalance = computed(
 
 <template>
   <!-- container -->
-  <div class="w-full max-w-screen-sm mx-auto px-4 py-12 grid gap-6">
+  <div class="w-full max-w-screen-sm mx-auto px-4 py-12 grid gap-4">
     <!-- balance section -->
     <section class="w-full bg-zinc-100 p-1 rounded-xl">
       <div
@@ -41,17 +41,37 @@ const availableBalance = computed(
     </section>
 
     <!-- diposit and withdraw section -->
-    <section class="w-full grid md:grid-cols-2 gap-6">
+    <section class="w-full grid md:grid-cols-2 gap-4">
       <!-- diposit section -->
       <div class="w-full bg-zinc-100 p-1 rounded-xl">
-        <div class="w-full h-full bg-white p-6 rounded-lg shadow">
-          Hello Deposit
+        <div class="w-full h-full bg-white p-6 rounded-lg shadow grid gap-4">
+          <h3 class="text-base font-medium">Add Deposite</h3>
+          <input
+            type="number"
+            class="w-full px-3 py-1.5 h-9 rounded-md bg-white border border-emerald-500 focus-visible:outline-none focus:ring-2 focus:ring-transparent focus:ring-offset-4 focus:ring-offset-emerald-100 text-xs"
+             placeholder="Enter your deposit amount"
+          />
+          <button
+            class="w-full px-3 py-1.5 h-9 rounded-md bg-emerald-500 text-white text-sm font-medium"
+          >
+            Add Deposite
+          </button>
         </div>
       </div>
       <!-- withdraw section -->
       <div class="w-full bg-zinc-100 p-1 rounded-xl">
-        <div class="w-full h-full bg-white p-6 rounded-lg shadow">
-          Hello WIthdraw
+        <div class="w-full h-full bg-white p-6 rounded-lg shadow grid gap-4">
+          <h3 class="text-base font-medium">Withdraw Request</h3>
+          <input
+            type="number"
+            class="w-full px-3 py-1.5 h-9 rounded-md bg-white border border-rose-500 focus-visible:outline-none focus:ring-2 focus:ring-transparent focus:ring-offset-4 focus:ring-offset-rose-100 text-xs"
+            placeholder="Enter your withdraw amount"
+          />
+          <button
+            class="w-full px-3 py-1.5 h-9 rounded-md bg-rose-500 text-white text-sm font-medium"
+          >
+            Make Withdraw
+          </button>
         </div>
       </div>
     </section>
