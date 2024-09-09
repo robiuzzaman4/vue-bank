ea8f0f41c8197aa8db7ab9e320702885fc59294a
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-09-09 21:53:35 +0600

    style: update balance section

--- src/components/Bank.vue
+++ src/components/Bank.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, computed } from "vue";
 
-const totalBalance = ref(100000);
+const totalBalance = ref(1009000);
 const accountCharge = computed(() => (totalBalance.value * 1.5) / 100); // 2.5% of the total
 const availableBalance = computed(
   () => totalBalance.value - accountCharge.value
@@ -16,15 +16,19 @@ const availableBalance = computed(
       <div
         class="w-full h-full bg-white p-6 rounded-lg shadow grid place-items-center gap-6"
       >
-        <h1 class="text-2xl text-center font-medium tracking-tighter">
+        <h1 class="text-2xl sm:text-3xl text-center font-medium tracking-tighter border-b border-b-zinc-200 w-full pb-6">
           Acc No: VB_09092024
         </h1>
-        <div class="w-full grid grid-cols-2 gap-2">
-          <span class="text-xl text-center font-medium tracking-tighter">
-            Total Balance: ${{ totalBalance.toLocaleString() }}
+        <div class="w-full flex items-center justify-between gap-2">
+          <span
+            class="text-base sm:text-xl md:text-2xl font-medium tracking-tighter flex flex-col gap-1"
+          >
+            Total Balance:
+            <p class="text-emerald-500">${{ totalBalance.toLocaleString() }}</p>
           </span>
-          <span class="text-xl text-center font-medium tracking-tighter">
-            Available Balance: ${{ availableBalance.toLocaleString() }}
+          <span class="text-base sm:text-xl md:text-2xl font-medium tracking-tighter flex flex-col gap-1">
+            Available Balance:
+            <p class="text-orange-500">${{ availableBalance.toLocaleString() }}</p>
           </span>
         </div>
       </div>
